1af2284b6b4ffd9c663a9530a522af985143dc10
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-06-03 22:44:23 +0100

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,28 +1,24 @@
-
-
-
-let calculation = JSON.parse(localStorage.getItem('calculation')) || '';
+let calculation = JSON.parse(localStorage.getItem('calculation')) ||  "";
 
 displayCalculation();
 
 function updateCalculation(value){
-   calculation += value;
-   displayCalculation();
-   saveCalculation();
-   
-    
-}
 
-function displayCalculation(){
-    document.querySelector('.display').innerHTML = calculation;
-}
+     calculation += value;
+     document.querySelector(".display").innerHTML = calculation;
 
-function saveCalculation(){
-    localStorage.setItem('calculation', JSON.stringify(calculation));
+     localStorage.setItem("calculation", JSON.stringify(calculation));
+
 }
 
 function clearCalculation(){
-    calculation = '';
-    displayCalculation();
-    saveCalculation();
+
+    calculation = 0;
+    localStorage.setItem("calculation", JSON.stringify(calculation));
+    document.querySelector(".display").innerHTML = "cleared";
+
+}
+
+function displayCalculation(){
+    document.querySelector(".display").innerHTML = calculation;
 }
