969e287c6ff1b368bc6811b9f31656096f5dc02d
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-06-20 20:04:20 +0100

    Add files via upload

--- bib.js
+++ bib.js
@@ -1,8 +1,10 @@
 let bib = JSON.parse(localStorage.getItem('bib')) || [{
-    code : 0,
-    title : 'The 48 laws of power',
-    author : 'Robert Green',
-    price : 3200
+
+    title : 'The Prince',
+    author : 'Niccolo Mechiavelli',
+    gender : 'Politics, history',
+    rate : 9,
+    state : 'Nread'
 }];
 
 
@@ -10,109 +12,161 @@ displayBib();
 
 function displayBib(){
 
-    let bibHtml= '';
 
-    for(let i = 0;i<bib.length;i++){
+    let container = document.querySelector(".display-content");
+    container.innerHTML = '';
 
-        let html = `
+    bib.forEach((book,i)=>{
+
+        const bookD = document.createElement("div");
+        bookD.className = "book-div";
         
-           <div class="book">
-              <p class="code">${bib[i].code}</p>
-              <p class="title">${bib[i].title}</p>
-               <p class="author">${bib[i].author}</p>  
-               <p class="price">${bib[i].price} Da</p>
 
-               <button class="delete-button" onclick="
-                  bib.splice(${i}, 1);
-                  displayBib();
-               ">
-                  Delete
-               </button>
+        const titleD = document.createElement("div");
+        titleD.className = "title";
+        titleD.textContent = `- Title : ${book.title}`;
 
-            </div>
-        
-        `;
+        const authorD = document.createElement("div");
+        authorD.className = "author";
+        authorD.textContent = `- Author : ${book.author}`;
 
-        bibHtml += html;
-    }
+        const genderD = document.createElement("div");
+        genderD.className = "gender";
+        genderD.textContent = `- Gender : ${book.gender}`;
 
-    document.querySelector(".display-content").innerHTML = bibHtml;
+        const rateD = document.createElement("div");
+        rateD.className = "rate";
+        rateD.textContent = `- Rate : ${book.rate}/10`;
+
+        const stateButton = document.createElement("button");
+        stateButton.className = "state-button";
+        if(book.state === 'read'){
+            stateButton.style.backgroundColor = "green";
+
+        }else{
+            stateButton.style.backgroundColor = "transparent";
+        }
+
+
+        stateButton.addEventListener('click', ()=>{
+            if(book.state === 'read'){
+                book.state = 'Nread';
+                stateButton.style.backgroundColor = "transparent";
+            }else{
+                book.state = 'read';
+                stateButton.style.backgroundColor = "green";
+            }
+
+            localStorage.setItem('bib', JSON.stringify(bib));
+        });
+
+
+        const deleteButton = document.createElement("button")
+        deleteButton.className = "delete-button";
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener('click', ()=>{
+            bib.splice(i , 1);
+            displayBib();
+        });
+
+
+        bookD.appendChild(titleD);
+        bookD.appendChild(authorD);
+        bookD.appendChild(genderD);
+        bookD.appendChild(rateD);
+        bookD.appendChild(stateButton);
+        bookD.appendChild(deleteButton);
+
+        container.appendChild(bookD);
+
+    });
+
     localStorage.setItem('bib', JSON.stringify(bib));
+
 
 }
 
-function addBib(){
 
+let timeId1;
+function addBook(){
 
-    let codeInput = document.querySelector(".input-code");
-    let titleInput = document.querySelector(".input-title");
-    let authorInput = document.querySelector(".input-author");
-    let priceInput = document.querySelector(".input-price");
+    
+    let titleIn = document.querySelector(".input-title");
+    let authorIn = document.querySelector(".input-author");
+    let genderIn = document.querySelector(".input-gender");
+    let rateIn = document.querySelector(".input-rate");
 
-    let code = codeInput.value;
-    let title = titleInput.value;
-    let author = authorInput.value;
-    let price = priceInput.value;
+    let title = titleIn.value;
+    let author = authorIn.value;
+    let gender = genderIn.value;
+    let rate = rateIn.value;
 
-    let trouve = false;
-    let i = 0;
+     let trouve = false;
+        let i = 0;
 
-    while(i<bib.length && (!trouve)){
+        while(i<bib.length && (!trouve)){
+            if(title.toLowerCase() === bib[i].title.toLowerCase()){
 
-        if(bib[i].code === Number(code)){
-            trouve = true;
-        }else{
-            i++;
+           trouve = true;
+            }else{
+                i++;
+            }
         }
-    }
 
-    if(trouve){
-        document.querySelector(".display-error-message").innerHTML = 'The code you entered already exists please try another one';
-    }else if(isNaN(code) || isNaN(price)){
-        document.querySelector(".display-error-message").innerHTML = 'The price and the code must contain only numbers !';
-    }else if(Number(price) < 0){
-         document.querySelector(".display-error-message").innerHTML = 'The price must be a positive number';
+        if(trouve){
+                          document.querySelector(".display-error-message").innerHTML = 'The book you entered already exists';
+        }else if(title === '' || author === ''|| gender === ''|| rate === ''){
+        document.querySelector(".display-error-message").innerHTML = 'Please enter all the necessary information';
+    }else if(isNaN(rate) || Number(rate) < 0|| Number(rate) > 10){
+                        document.querySelector(".display-error-message").innerHTML = 'Please the rate must be a number between 0 and 10';
+
     }else{
 
-         document.querySelector(".display-error-message").innerHTML = '';
+                      document.querySelector(".display-error-message").innerHTML = '';
 
-         bib.push({
-            code : Number(code),
+        bib.push({
             title,
             author,
-            price : Number(price)
-         });
+            gender,
+            rate : Number(rate),
+            state : 'Ndone'
+        });
 
-         displayBib();
+        displayBib();
 
-          document.querySelector(".display-success-message").innerHTML = 'Book added successfuly';
+            document.querySelector(".display-success-message").innerHTML = 'Book entered with success';
+        clearTimeout(timeId1);
+        timeId1 = setTimeout(()=>{
+            document.querySelector(".display-success-message").innerHTML = '';
+        }, 3000);
 
-          setTimeout(()=>{
-             document.querySelector(".display-success-message").innerHTML = '';
-          }, 3000);
+        titleIn.value = '';
+        authorIn.value = '';
+        genderIn.value = '';
+        rateIn.value = '';
 
-          codeInput.value = '';
-          titleInput.value = '';
-          authorInput.value = '';
-          priceInput.value = '';
     }
 }
 
 document.querySelector(".add-button").addEventListener('click', ()=>{
-    addBib();
+    addBook();
 });
 
 
-function searchBib(){
+let timeId2;
+let timeId3;
+
+function searchBook(){
+
+    let titleIn = document.querySelector(".search-code");
+
+    let title = titleIn.value;
 
     let trouve = false;
     let i = 0;
 
-    let code = (document.querySelector(".search-code")).value;
-
     while(i<bib.length && (!trouve)){
-
-        if(bib[i].code === Number(code)){
+        if(title.toLowerCase() === bib[i].title.toLowerCase()){
             trouve = true;
         }else{
             i++;
@@ -120,251 +174,235 @@ function searchBib(){
     }
 
     if(trouve){
+        document.querySelector(".search-result").innerHTML = 'Book found !';
 
-        document.querySelector(".search-message").innerHTML = 'Book found';
+        clearTimeout(timeId2);
 
+        timeId2 = setTimeout(()=>{
+            document.querySelector(".search-result").innerHTML = '';
+        },3000);
+
+        const container = document.querySelector(".display-search");
+        container.innerHTML = '';
+
+            const bookD = document.createElement("div");
+        bookD.className = "book-div";
+        
+
+        const titleD = document.createElement("div");
+        titleD.className = "title";
+        titleD.textContent = `- Title : ${bib[i].title}`;
+
+        const authorD = document.createElement("div");
+        authorD.className = "author";
+        authorD.textContent = `- Author : ${bib[i].author}`;
+
+        const genderD = document.createElement("div");
+        genderD.className = "gender";
+        genderD.textContent = `- Gender : ${bib[i].gender}`;
+
+        const rateD = document.createElement("div");
+        rateD.className = "rate";
+        rateD.textContent = `- Rate : ${bib[i].rate}/10`;
+
+        bookD.appendChild(titleD);
+        bookD.appendChild(authorD);
+        bookD.appendChild(genderD);
+        bookD.append(rateD);
+
+        container.appendChild(bookD);
+
+        clearInterval(timeId3);
         setTimeout(()=>{
-            document.querySelector(".search-message").innerHTML = '';
-        }, 3000);
+           container.innerHTML = '';
+        }, 4000);
 
-        document.querySelector(".s-title").innerHTML = `${bib[i].title}`;
-        document.querySelector(".s-author").innerHTML = `${bib[i].author}`;
-        document.querySelector(".s-price").innerHTML = `${bib[i].price}`;
-
-        setTimeout(()=>{
-            document.querySelector(".s-title").innerHTML = '';
-        document.querySelector(".s-author").innerHTML = '';
-        document.querySelector(".s-price").innerHTML =  '';
-        }, 5000);
-
-      (document.querySelector(".search-code")).value = '';
-
+        titleIn.value = '';
 
     }else{
-        document.querySelector(".search-message").innerHTML = 'The code you entered was not found';
+        document.querySelector(".search-result").innerHTML = 'Book not found !';
 
-        setTimeout(()=>{
-            document.querySelector(".search-message").innerHTML = '';
-        }, 3000);
+        clearTimeout(timeId2);
+        
+        timeId2 = setTimeout(()=>{
+            document.querySelector(".search-result").innerHTML = '';
+        },3000);
+
     }
+
 }
 
 
 document.querySelector(".search-button").addEventListener('click', ()=>{
-    searchBib();
+    searchBook();
+});
+
+
+function modifBook(){
+
+        let titleIn = document.querySelector(".m-title");
+    let authorIn = document.querySelector(".m-author");
+    let genderIn = document.querySelector(".m-gender");
+    let rateIn = document.querySelector(".m-rate");
+
+    let title = titleIn.value;
+    let author = authorIn.value;
+    let gender = genderIn.value;
+    let rate = rateIn.value;
+
+    let trouve = false;
+    let i = 0;
+
+    while(i<bib.length && (!trouve)){
+
+        if(title.toLowerCase() === bib[i].title.toLowerCase()){
+            trouve = true;
+        }else{
+            i++;
+        }
+    }
+
+    if(!trouve){
+          document.querySelector(".modif-result").innerHTML = 'Book not found';
+    }else if(title === ''|| author === '' || gender === ''|| rate === ''){
+        document.querySelector(".modif-result").innerHTML = 'Please enter all the necessary information';
+    }else if(isNaN(rate) || Number(rate) < 0 || Number(rate)>20){
+        document.querySelector(".modif-result").innerHTML = 'Please the rate must be a number between 0 and 20';
+    }else{
+
+        bib[i].title = title;
+        bib[i].author = author;
+        bib[i].gender = gender;
+        bib[i].rate = Number(rate);
+        bib[i].state = 'Ndone';
+
+        displayBib();
+
+        document.querySelector(".modif-result").innerHTML = 'Modification done with succes';
+
+        setTimeout(()=>{
+            document.querySelector(".modif-result").innerHTML = '';
+        }, 3000);
+
+        titleIn.value = '';
+        authorIn.value = '';
+        genderIn.value = '';
+        rateIn.value = '';
+    }
+}
+
+
+document.querySelector(".modif-button").addEventListener('click', ()=>{
+    modifBook();
+});
+
+let timeId4;
+function displayNbrBooks(){
+
+    clearTimeout(timeId4);
+
+    document.querySelector(".display-nbr").innerHTML = `${bib.length}`;
+
+    timeId4 = setTimeout(()=>{
+        document.querySelector(".display-nbr").innerHTML = '';
+    }, 3000);
+    
+}
+
+
+document.querySelector(".nbr-button").addEventListener('click', ()=>{
+    displayNbrBooks();
+});
+
+let timeId5;
+function displayNbrRead(){
+
+    let cpt = 0;
+
+    bib.forEach((book, i)=>{
+        if(book.state === 'read'){
+            cpt++;
+        }
+    });
+
+    clearInterval(timeId5);
+
+    document.querySelector(".display-read").innerHTML = `${cpt}`;
+
+    timeId5 = setTimeout(()=>{
+        document.querySelector(".display-read").innerHTML = '';
+    },3000);
+
+    
+}
+
+document.querySelector(".read-button").addEventListener('click', ()=>{
+    displayNbrRead();
+});
+
+
+let timeId6;
+function displayNbrNread(){
+
+    let cpt = 0;
+
+    bib.forEach((book, i)=>{
+        if(book.state !== 'read'){
+            cpt++;
+        }
+    });
+
+
+    document.querySelector(".display-Nread").innerHTML = `${cpt}`;
+
+    timeId6 = setTimeout(()=>{
+         document.querySelector(".display-Nread").innerHTML = '';
+    },3000);
+
+
+}
+
+document.querySelector(".Nread-button").addEventListener('click', ()=>{
+    displayNbrNread();
 });
 
 
 function calculateMoy(){
 
-    let S = 0;
-    let cpt = 0;
     let moy = 0;
+    let cpt = 0,S = 0;
 
-    for(let i = 0;i<bib.length;i++){
-        S += bib[i].price;
+    bib.forEach((book)=>{
+        S += Number(book.rate);
         cpt++;
+    });
 
-    }
-
-    if(cpt != 0){
-        moy = S/cpt;
-    }
+    moy = S/cpt;
 
     return moy.toFixed(2);
+
 }
 
-function displaymoy(){
+
+let timeId7;
+function displayMoy(){
+
 
     let moy = calculateMoy();
-    document.querySelector(".display-moy").innerHTML = `${moy} Da`;
 
-    setTimeout(()=>{
-        document.querySelector(".display-moy").innerHTML = '';
+    document.querySelector(".display-rate").innerHTML = `${moy}/10`;
+
+
+    clearTimeout(timeId7);
+
+    timeId7 = setTimeout(()=>{
+        document.querySelector(".display-rate").innerHTML = '';
     }, 3000);
 
-}
-
-document.querySelector(".moy-button").addEventListener('click', ()=>{
-    displaymoy();
-});
-
-
-function findMax(){
-
-    let max = bib[0];
-
-    for(let i = 1;i<bib.length;i++){
-        if(max.price < bib[i].price){
-            max = bib[i];
-        }
-    }
-
-    return max;
-
-    
-}
-
-function displayMax(){
-
-    let max = findMax();
-
-    if(max != null){
-
-        document.querySelector(".max-code").innerHTML = `${max.code}`;
-        document.querySelector(".max-title").innerHTML = `${max.title}`;
-        document.querySelector(".max-author").innerHTML = `${max.author}`;
-        document.querySelector(".max-price").innerHTML = `${max.price}`;
-
-        setTimeout(()=>{
-                    document.querySelector(".max-code").innerHTML = '';
-        document.querySelector(".max-title").innerHTML =  '';
-        document.querySelector(".max-author").innerHTML = '';
-        document.querySelector(".max-price").innerHTML = '';
-
-        }, 5000);
-    }
-}
-
-document.querySelector(".max-button").addEventListener('click', ()=>{
-    displayMax();
-});
-
-function findMin(){
-
-    let min = bib[0];
-
-    for(let i = 0;i<bib.length;i++){
-
-        if(bib[i].price < min.price){
-            min = bib[i];
-        }
-
-    }
-
-    return min;
-}
-
-function displayMin(){
-
-    let min = findMin();
-
-    if(min != null){
-        document.querySelector(".min-code").innerHTML = `${min.code}`;
-        document.querySelector(".min-title").innerHTML = `${min.title}`;
-        document.querySelector(".min-author").innerHTML = `${min.author}`;
-        document.querySelector(".min-price").innerHTML = `${min.price}`;
-
-
-          setTimeout(()=>{
-                            document.querySelector(".min-code").innerHTML = '';
-        document.querySelector(".min-title").innerHTML = '';
-        document.querySelector(".min-author").innerHTML ='';
-        document.querySelector(".min-price").innerHTML = '';
-          }, 5000);
-
-    }
-}
-
-document.querySelector(".min-button").addEventListener('click', ()=>{
-    displayMin();
-});
-
-
-
-function modifyBib(){
-
-    let codeInput = document.querySelector(".modif-code");
-    let titleInput = document.querySelector(".modif-title");
-    let authorInput = document.querySelector(".modif-author");
-    let priceInput = document.querySelector(".modif-price");
-
-    let code = codeInput.value;
-    let title = titleInput.value;
-    let author = authorInput.value;
-    let price = priceInput.value;
-
-    let trouve = false;
-    let i = 0;
-    if(code === '' || title === '' || author === '' || price === ''){
-        document.querySelector(".modif-result").innerHTML = 'Enter all the correct information';
-    }else if(Number(price) < 0){
-        document.querySelector(".modif-result").innerHTML = 'The price must be a positive number';
-    }else{
-
-        document.querySelector(".modif-result").innerHTML = '';
-
-    while(i<bib.length && (!trouve)){
-        if(bib[i].code === Number(code)){
-            bib[i].title = title;
-            bib[i].author = author;
-            bib[i].price = Number(price);
-            trouve = true;
-        }else{
-            i++;
-        }
-    }
-
-    if(trouve){
-        document.querySelector(".modif-result").innerHTML = 'Modification done with success';
-
-        displayBib();
-    }else{
-        document.querySelector(".modif-result").innerHTML = 'Code not found';
-    }
-
-    codeInput.value = '';
-    titleInput.value = ''
-    authorInput.value = '';
-    priceInput.value = '';
-}
-
 
 }
 
-
-document.querySelector(".modif-button").addEventListener('click', ()=>{
-    modifyBib();
-});
-
-function sortBib(){
-
-    for(let i = 0;i<bib.length-1;i++){
-        for(let j = i+1;j<bib.length;j++){
-
-            if(bib[i].price > bib[j].price){
-                let temp = bib[i];
-                bib[i] = bib[j];
-                bib[j] = temp;
-            }
-        }
-    }
-
-    let sortHtml = '';
-
-    for(let i = 0;i<bib.length;i++){
-
-          let html = `
-        
-           <div class="book">
-              <p class="code">${bib[i].code}</p>
-              <p class="title">${bib[i].title}</p>
-               <p class="author">${bib[i].author}</p>  
-               <p class="price">${bib[i].price} Da</p>
-
-            </div>
-        
-        `;
-        
-        sortHtml += html;
-    }
-
-    document.querySelector(".display-sort").innerHTML = sortHtml;
-
-
-}
-
-document.querySelector(".sort-button").addEventListener('click', ()=>{
-    sortBib();
+document.querySelector(".rate-button").addEventListener('click', ()=>{
+    displayMoy();
 });
 
