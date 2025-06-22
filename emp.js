0344a0941560a4e844079fc59d80057bd41682fb
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-06-22 12:34:12 +0100

    Add files via upload

--- emp.js
+++ emp.js
@@ -128,6 +128,297 @@ function addEmp(){
 
 }
 
-document.querySelector(".add-button").addEventListener('click', ()=>{
+let addTime;
+
+const addButton = document.querySelector(".add-button");
+
+
+addButton.addEventListener('click', ()=>{
+   
+    addButton.disabled = true;
+
     addEmp();
+
+    clearTimeout(addTime);
+
+    addTime = setTimeout(()=>{
+        addButton.disabled = false;
+    }, 500);
 });
+
+let timeId1;
+let timeId2;
+function searchEmp(){
+
+    let codeIn = document.querySelector(".search-code");
+    let code = codeIn.value;
+
+    let trouve = false;
+    let i = 0;
+
+    while(i<emp.length && (!trouve)){
+
+        if(emp[i].code === code){
+            trouve = true;
+
+        }else{
+            i++;
+        }
+    }
+
+    if(trouve){
+
+        document.querySelector(".search-message").innerHTML = 'Employee found !';
+
+        clearTimeout(timeId1);
+
+        timeId1 = setTimeout(()=>{
+             document.querySelector(".search-message").innerHTML = '';
+        }, 2500);
+
+
+        let container = document.querySelector(".display-search");
+        container.innerHTML = '';
+
+        const empD = document.createElement("div");
+        empD.className = "emp-div";
+
+        const codeD = document.createElement("div");
+        codeD.textContent = `- Code : ${emp[i].code}`;
+
+        const nomD = document.createElement("div");
+        nomD.textContent = `- Last name : ${emp[i].nom}`;
+
+        const prenomD = document.createElement("div");
+        prenomD.textContent = `- First name : ${emp[i].prenom}`;
+
+        const departementD = document.createElement("div");
+        departementD.textContent = `- Departement : ${emp[i].departement}`;
+
+        const posteD = document.createElement("div");
+        posteD.textContent = `- Poste : ${emp[i].poste}`;
+
+        empD.appendChild(codeD);
+        empD.appendChild(nomD);
+        empD.appendChild(prenomD);
+        empD.appendChild(departementD);
+        empD.appendChild(posteD);
+
+        container.appendChild(empD);
+
+        clearTimeout(timeId2);
+
+        timeId2 = setTimeout(()=>{
+            container.innerHTML = '';
+        },4000);
+
+        codeIn.value = '';
+        
+    }else{
+
+        document.querySelector(".search-message").innerHTML = 'Employee not found';
+
+        clearTimeout(timeId1);
+
+        timeId1 = setTimeout(()=>{
+            document.querySelector(".search-message").innerHTML = '';
+        }, 2500);
+
+    }
+}
+
+
+document.querySelector(".search-button").addEventListener('click', ()=>{
+    searchEmp();
+});
+
+
+let timeId3;
+function modifEmp(){
+
+    let codeIn = document.querySelector(".m-code");
+    let nomIn = document.querySelector(".m-nom");
+    let prenomIn = document.querySelector(".m-prenom");
+    let departementIn = document.querySelector(".m-departement");
+    let posteIn = document.querySelector(".m-poste");
+
+    let code = codeIn.value;
+    let nom = nomIn.value;
+    let prenom = prenomIn.value;
+    let departement = departementIn.value;
+    let poste = posteIn.value;
+
+    let trouve = false;
+    let i = 0;
+
+    while(i<emp.length && (!trouve)){
+
+        if(emp[i].code === code){
+            trouve = true;
+
+        }else{
+            i++;
+        }
+
+    }
+
+    if(!trouve){
+        document.querySelector(".modif-message").innerHTML = 'Employee not found';
+    }else if(code === '' || prenom === '' || nom === '' || departement === '' || poste === ''){
+        document.querySelector(".modif-message").innerHTML = 'Please enter all the necessary information';
+    }else{
+
+        document.querySelector(".modif-message").innerHTML = '';
+
+        emp[i].nom = nom;
+        emp[i].prenom = prenom;
+        emp[i].departement = departement;
+        emp[i].poste = poste;
+
+        displayEmp();
+
+        document.querySelector(".modif-message").innerHTML = 'Modification done with success';
+
+        clearInterval(timeId3);
+
+        timeId3 = setTimeout(()=>{
+            document.querySelector(".modif-message").innerHTML = '';
+        }, 2000);
+
+        codeIn.value = '';
+        nomIn.value = '';
+        prenomIn.value = '';
+        departementIn.value = '';
+        posteIn.value = '';
+    }
+}
+
+
+const modifButton = document.querySelector(".modif-button");
+
+
+modifButton.addEventListener('click', ()=>{
+
+    modifButton.disabled = true;
+
+    modifEmp();
+
+    clearTimeout(addTime);
+
+    addTime = setTimeout(()=>{
+       modifButton.disabled = false;
+    }, 500);
+
+});
+
+
+let totalTime;
+
+function displayTotalEmp(){
+
+
+    document.querySelector(".display-total").innerHTML = `${emp.length}`;
+
+    clearTimeout(totalTime);
+
+    totalTime = setTimeout(()=>{
+        document.querySelector(".display-total").innerHTML = '';
+    }, 3000);
+}
+
+
+document.querySelector(".total-button").addEventListener('click', ()=>{
+    displayTotalEmp();
+});
+
+
+
+function nbrHr(){
+
+    let cpt = 0;
+
+    emp.forEach((element)=>{
+        if(element.departement === 'HR' ){
+            cpt++;
+        }
+    });
+
+    return cpt;
+}
+
+
+function nbrLog(){
+
+    let cpt = 0;
+
+        emp.forEach((element)=>{
+        if(element.departement === 'Logistics' ){
+            cpt++;
+        }
+    });
+
+    return cpt;
+}
+
+
+function nbrPro(){
+
+    let cpt = 0;
+
+        emp.forEach((element)=>{
+        if(element.departement === 'Projects' ){
+            cpt++;
+        }
+    });
+
+    return cpt;
+}
+
+
+function nbrTec(){
+
+    let cpt = 0;
+
+        emp.forEach((element)=>{
+        if(element.departement === 'Technical' ){
+            cpt++;
+        }
+    });
+
+    return cpt;
+}
+
+
+function nbrF(){
+
+    let cpt = 0;
+
+
+   emp.forEach((element)=>{
+        if(element.departement === 'Finance' ){
+            cpt++;
+        }
+    });
+
+    return cpt;
+}
+
+
+
+function displayNbr(){
+
+    let nbrH = nbrHr();
+    let nbrP = nbrPro();
+    let nbrFi = nbrF();
+    let nbrL = nbrLog();
+    let nbrT = nbrTec();
+
+
+    document.querySelector(".nbr-hr").innerHTML = `${nbrH}`;
+    document.querySelector(".nbr-pro").innerHTML = `${nbrP}`;
+    document.querySelector(".nbr-log").innerHTML= `${nbrL}`;
+    document.querySelector(".nbr-fi").innerHTML = `${nbrFi}`;
+    document.querySelector(".nbr-tec").innerHTML = `${nbrT}`;
+}
+
+displayNbr();
