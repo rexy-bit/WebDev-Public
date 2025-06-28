f64f316d4dc6afb148569461a2a207b3456a60cf
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-06-28 22:25:58 +0100

    Add files via upload

--- emp.js
+++ emp.js
@@ -1,424 +1,363 @@
-let emp = JSON.parse(localStorage.getItem('emp')) || [{
-    code: '0',
-    nom : 'Rezgui',
-    prenom : 'Yanis',
-    departement : 'Projects',
-    poste : 'Interne'
-}];
+const navBar = document.querySelector("nav");
+const humburger = document.querySelector(".humburger");
+
+navBar.style.display = "none";
+
+humburger.addEventListener('click', ()=>{
+    if(navBar.style.display === 'none'){
+        navBar.style.display = "flex";
+    }else{
+        navBar.style.display = "none";
+    }
+});
 
 
-displayEmp();
 function displayEmp(){
 
-    const container = document.querySelector(".display-content");
-    container.innerHTML = '';
+     fetch('http://localhost:3000/employes')
+     .then(response=>response.json())
+     .then(data=>{
+        console.log(data);
 
-    emp.forEach((element, i)=>{
+        const container = document.querySelector(".display-content");
+        container.innerHTML = '';
 
-        const empDiv = document.createElement("div");
-        empDiv.className = "emp-div";
+        data.forEach(emp => {
         
-        const codeD = document.createElement("div");
-        codeD.textContent = `- Code : ${element.code}`;
+            const empDiv = document.createElement("div");
+            empDiv.className = "emp-div";
 
-        const nomD = document.createElement("div");
-        nomD.className = "nom";
-        nomD.textContent = `- Last name : ${element.nom}`;
+            const codeD = document.createElement("div");
+            codeD.textContent = `- Code : ${emp.code}`;
 
-        const prenomD = document.createElement("div");
-        prenomD.textContent = `- Prenom : ${element.prenom}`;
+            const nomD = document.createElement("div");
+            nomD.textContent = `- Last name : ${emp.nom}`
 
-        const departementD = document.createElement("div");
-        departementD.textContent = `- Departement : ${element.departement}`;
+            const prenomD = document.createElement("div");
+            prenomD.textContent = `-First name : ${emp.prenom}`;
 
-        const posteD = document.createElement("div");
-        posteD.textContent = `- Poste : ${element.poste}`;
+            const posteD = document.createElement("div");
+            posteD.textContent = `- Poste : ${emp.poste}`;
 
-        const deleteButton = document.createElement("button");
-        deleteButton.className = "delete-button";
-        deleteButton.textContent = "Delete";
+            const salaireD = document.createElement("div");
+            salaireD.textContent = `- Salary : ${emp.salaire} Da`;
 
-        deleteButton.addEventListener('click', ()=>{
-            emp.splice(i, 1);
-            displayEmp();
+            const deleteButton = document.createElement("button");
+            deleteButton.className = "delete-button";
+            deleteButton.textContent = "Delete";
+
+            deleteButton.addEventListener('click', ()=>{
+                if(confirm(`Are you sure to delete employe with id : ${emp.id} ?`)){
+                    deleteEmp(emp.id);
+                }
+            });
+
+            empDiv.appendChild(codeD);
+            empDiv.appendChild(nomD);
+            empDiv.appendChild(prenomD);
+            empDiv.appendChild(posteD);
+            empDiv.appendChild(salaireD);
+            empDiv.appendChild(deleteButton);
+
+   container.appendChild(empDiv);
         });
+     })
+      .catch(error=>console.error('Error in display : ', error));
+     
+}
+
+displayEmp();
 
 
-        empDiv.appendChild(codeD);
-        empDiv.appendChild(nomD);
-        empDiv.appendChild(prenomD)
-        empDiv.appendChild(departementD);
-        empDiv.appendChild(posteD);
-        empDiv.appendChild(deleteButton);
+function deleteEmp(id){
 
-
-        container.appendChild(empDiv);
-        });
-
-        localStorage.setItem('emp', JSON.stringify(emp));
+    fetch(`http://localhost:3000/employes/${id}`, {
+        method : 'DELETE'
+    }).then(()=>{
+        console.log(`Employe avec Id : ${id} Supprime`);
+        displayEmp();
+    }
+    ).catch(error=> console.log('Error in deleting : ', error));
 
 }
 
-let timeId;
+
+let addTimeout;
 function addEmp(){
 
 
     let codeIn = document.querySelector(".input-code");
     let nomIn = document.querySelector(".input-nom");
     let prenomIn = document.querySelector(".input-prenom");
-    let departementIn = document.querySelector(".input-departement");
     let posteIn = document.querySelector(".input-poste");
+    let salaireIn = document.querySelector(".input-salaire");
 
     let code = codeIn.value;
     let nom = nomIn.value;
     let prenom = prenomIn.value;
-    let departement = departementIn.value;
     let poste = posteIn.value;
+    let salaire = salaireIn.value;
 
 
     let trouve = false;
-    let i = 0;
-    
-    while(i<emp.length && (!trouve)){
 
-        if(emp[i].code === code){
-            trouve = true;
+    const msg = document.querySelector(".add-message");
+    msg.style.color = "red";
+
+    fetch('http://localhost:3000/employes')
+    .then(response =>response.json())
+    .then(data=>{
+
+        data.forEach((emp)=>{
+            if(emp.code === code){
+                trouve = true;
+                return;
+            }
+        });
+
+
+        if(trouve){
+            msg.innerHTML = 'The code you entered already exists please try another one';
+        }else if(code === '' || nom === '' || prenom === '' || poste === '' || salaire === ''){
+            msg.innerHTML = 'Please enter all the necessary information';
+        }else if(isNaN(salaire) || Number(salaire) < 0){
+              msg.innerHTML = 'The salary must be a positive number';
         }else{
-            i++;
+
+            msg.innerHTML = '';
+
+            const newEmp = {
+                code,
+                nom,
+                prenom,
+                poste,
+                salaire : Number(salaire)
+            };
+
+            fetch('http://localhost:3000/employes', {
+                method : 'POST',
+                headers : {
+                    'Content-type' : 'application/json'
+                },
+                body : JSON.stringify(newEmp)
+            }).then(response => response.json())
+                .then(data=>{
+                    console.log('Employe added with success : ', data);
+                    displayEmp();
+                    codeIn.value = '';
+                    nomIn.value = '';
+                    prenomIn.value ='';
+                    posteIn.value = '';
+                    salaireIn.value ='';
+
+                    clearTimeout(addTimeout);
+
+                    msg.style.color = "green";
+
+                    msg.innerHTML = 'Employe added with success';
+
+                    addTimeout = setTimeout(()=>{
+                        msg.innerHTML= '';
+                    }, 2000);
+
+                }).catch(error=> console.error('Erreur lors de l\'ajout : ', error));
         }
-    }
+    })
+    .catch(error=>console.error('Error in cheking the existence of codeIn : ', error));
 
-    if(trouve){
-
-        document.querySelector(".display-error-message").innerHTML  = 'The code you entered already exists';
-    }else if(code === '' || nom === '' || prenom === '' || departement === '' || poste === ''){
-
-         document.querySelector(".display-error-message").innerHTML  = 'Please enter all the necessary information';
-        
-    }else{
-
-         document.querySelector(".display-error-message").innerHTML  = '';
-
-         emp.push({
-            code,
-            nom,
-            prenom,
-            departement,
-            poste
-         });
-
-         displayEmp();
-
-
-         document.querySelector(".display-success-message").innerHTML  = 'Employee added with success';
-
-         clearTimeout(timeId);
-
-         timeId = setTimeout(()=>{
-             document.querySelector(".display-success-message").innerHTML  = '';
-        }, 2000);
-
-        codeIn.value = '';
-        nomIn.value = '';
-        prenomIn.value = '';
-        departementIn.value = '';
-        posteIn.value = '';
-        
-    }
 
 }
 
-let addTime;
 
 const addButton = document.querySelector(".add-button");
 
-
+let addButtonTimeout;
 addButton.addEventListener('click', ()=>{
-   
-    addButton.disabled = true;
+     addButton.disabled = true;
 
-    addEmp();
+     addEmp();
 
-    clearTimeout(addTime);
+     clearTimeout(addButtonTimeout);
 
-    addTime = setTimeout(()=>{
+     addButtonTimeout = setTimeout(()=>{
         addButton.disabled = false;
-    }, 500);
+     }, 500);
 });
 
-let timeId1;
-let timeId2;
+
+let searchTimeout;
+
+let containerTimeout;
 function searchEmp(){
+
+    let trouve = false;
+    let msg = document.querySelector(".search-message");
 
     let codeIn = document.querySelector(".search-code");
     let code = codeIn.value;
 
-    let trouve = false;
-    let i = 0;
 
-    while(i<emp.length && (!trouve)){
+    fetch('http://localhost:3000/employes')
+     .then(response => response.json())
+       .then(data=>{
 
-        if(emp[i].code === code){
-            trouve = true;
+        console.log(data);
+        let findEmp;
 
-        }else{
-            i++;
-        }
-    }
+          data.forEach(emp=>{
+             if(emp.code === code){
+                trouve = true;
+                findEmp = emp;
+                return;
+             }
+          });
 
-    if(trouve){
+          if(trouve){
+             msg.style.color = "green";
+             msg.innerHTML = "employe found !";
 
-        document.querySelector(".search-message").innerHTML = 'Employee found !';
+             clearTimeout(searchTimeout);
 
-        clearTimeout(timeId1);
+             searchTimeout = setTimeout(()=>{
+                msg.innerHTML = '';
+             }, 2000);
 
-        timeId1 = setTimeout(()=>{
-             document.querySelector(".search-message").innerHTML = '';
-        }, 2500);
+             const container = document.querySelector(".display-search");
+             container.innerHTML = '';
 
+               const empDiv = document.createElement("div");
+            empDiv.className = "emp-div";
 
-        let container = document.querySelector(".display-search");
-        container.innerHTML = '';
+            const codeD = document.createElement("div");
+            codeD.textContent = `- Code : ${findEmp.code}`;
 
-        const empD = document.createElement("div");
-        empD.className = "emp-div";
+            const nomD = document.createElement("div");
+            nomD.textContent = `- Last name : ${findEmp.nom}`
 
-        const codeD = document.createElement("div");
-        codeD.textContent = `- Code : ${emp[i].code}`;
+            const prenomD = document.createElement("div");
+            prenomD.textContent = `-First name : ${findEmp.prenom}`;
 
-        const nomD = document.createElement("div");
-        nomD.textContent = `- Last name : ${emp[i].nom}`;
+            const posteD = document.createElement("div");
+            posteD.textContent = `- Poste : ${findEmp.poste}`;
 
-        const prenomD = document.createElement("div");
-        prenomD.textContent = `- First name : ${emp[i].prenom}`;
+            const salaireD = document.createElement("div");
+            salaireD.textContent = `- Salary : ${findEmp.salaire} Da`;
 
-        const departementD = document.createElement("div");
-        departementD.textContent = `- Departement : ${emp[i].departement}`;
+            empDiv.appendChild(codeD);
+            empDiv.appendChild(nomD);
+            empDiv.appendChild(prenomD);
+            empDiv.appendChild(posteD);
+            empDiv.appendChild(salaireD);
+            
+            container.appendChild(empDiv);
 
-        const posteD = document.createElement("div");
-        posteD.textContent = `- Poste : ${emp[i].poste}`;
+            clearTimeout(containerTimeout);
 
-        empD.appendChild(codeD);
-        empD.appendChild(nomD);
-        empD.appendChild(prenomD);
-        empD.appendChild(departementD);
-        empD.appendChild(posteD);
+            containerTimeout = setTimeout(()=>{
+                container.innerHTML = '';
+            }, 4000);
 
-        container.appendChild(empD);
+            codeIn.value = '';
+          }else{
+            msg.style.color = "red";
+            msg.innerHTML = 'Code not found';
+          }
 
-        clearTimeout(timeId2);
+       }).catch(error=>console.error('Error is search function : ', error));
 
-        timeId2 = setTimeout(()=>{
-            container.innerHTML = '';
-        },4000);
-
-        codeIn.value = '';
-        
-    }else{
-
-        document.querySelector(".search-message").innerHTML = 'Employee not found';
-
-        clearTimeout(timeId1);
-
-        timeId1 = setTimeout(()=>{
-            document.querySelector(".search-message").innerHTML = '';
-        }, 2500);
-
-    }
 }
 
 
-document.querySelector(".search-button").addEventListener('click', ()=>{
+const searchButton = document.querySelector(".search-button");
+
+searchButton.addEventListener('click', ()=>{
     searchEmp();
 });
 
 
-let timeId3;
+let modifTimeout;
+
 function modifEmp(){
 
     let codeIn = document.querySelector(".m-code");
     let nomIn = document.querySelector(".m-nom");
     let prenomIn = document.querySelector(".m-prenom");
-    let departementIn = document.querySelector(".m-departement");
     let posteIn = document.querySelector(".m-poste");
+    let salaireIn = document.querySelector(".m-salaire");
 
     let code = codeIn.value;
     let nom = nomIn.value;
     let prenom = prenomIn.value;
-    let departement = departementIn.value;
     let poste = posteIn.value;
+    let salaire = salaireIn.value;
 
-    let trouve = false;
-    let i = 0;
 
-    while(i<emp.length && (!trouve)){
+    const msg = document.querySelector(".modif-message");
+    msg.style.color = "red";
+    
+    fetch('http://localhost:3000/employes')
+     .then(response =>response.json())
+     .then(data=>{
 
-        if(emp[i].code === code){
-            trouve = true;
+         let trouve = data.find(emp=> emp.code === code);
 
-        }else{
-            i++;
-        }
+         if(!trouve){
+            msg.innerHTML = 'Code not found';
+         }else if(code === '' || nom === '' || prenom === '' || poste === '' || salaire === ''){
+             msg.innerHTML = 'Enter all the necessary information';
+         }else if(isNaN(salaire) || Number(salaire) < 0){
+            msg.innerHTML = 'Please the salary must be a positive number';
+         }else{
 
-    }
+            msg.innerHTML = '';
 
-    if(!trouve){
-        document.querySelector(".modif-message").innerHTML = 'Employee not found';
-    }else if(code === '' || prenom === '' || nom === '' || departement === '' || poste === ''){
-        document.querySelector(".modif-message").innerHTML = 'Please enter all the necessary information';
-    }else{
+            const empModif = {
+                code,
+                nom,
+                prenom,
+                poste,
+                salaire : Number(salaire)
+            };
 
-        document.querySelector(".modif-message").innerHTML = '';
+            fetch(`http://localhost:3000/employes/${trouve.id}`, {
+                method : 'PUT',
+                headers : {
+                    'Content-type' : 'application/json'
+                },
+                body : JSON.stringify(empModif)
+            })
+            .then(response => {
+                if(response.ok){
+                    msg.style.color = "green";
 
-        emp[i].nom = nom;
-        emp[i].prenom = prenom;
-        emp[i].departement = departement;
-        emp[i].poste = poste;
+                    clearTimeout(modifTimeout);
+                    msg.innerHTML = "Employee updated successfuly";
 
-        displayEmp();
+                    modifTimeout = setTimeout(()=>{
+                        msg.innerHTML = '';
+                    }, 2000);
+                    displayEmp();
+                }else{
+                    msg.style.color = "red";
+                    msg.innerHTML = "an error occured";
+                }
+            })
+            .catch(error=>{
+                console.error('Error in update : ', error);
 
-        document.querySelector(".modif-message").innerHTML = 'Modification done with success';
+                msg.style.color = "red";
+                msg.innerHTML = "Error in update check the console";
+            });
+         }
+     }).catch(error=>console.error('Error in fetching data : ', error));
 
-        clearInterval(timeId3);
-
-        timeId3 = setTimeout(()=>{
-            document.querySelector(".modif-message").innerHTML = '';
-        }, 2000);
-
-        codeIn.value = '';
-        nomIn.value = '';
-        prenomIn.value = '';
-        departementIn.value = '';
-        posteIn.value = '';
-    }
 }
 
 
 const modifButton = document.querySelector(".modif-button");
 
-
 modifButton.addEventListener('click', ()=>{
-
-    modifButton.disabled = true;
-
     modifEmp();
-
-    clearTimeout(addTime);
-
-    addTime = setTimeout(()=>{
-       modifButton.disabled = false;
-    }, 500);
-
 });
 
 
-let totalTime;
-
-function displayTotalEmp(){
-
-
-    document.querySelector(".display-total").innerHTML = `${emp.length}`;
-
-    clearTimeout(totalTime);
-
-    totalTime = setTimeout(()=>{
-        document.querySelector(".display-total").innerHTML = '';
-    }, 3000);
-}
-
-
-document.querySelector(".total-button").addEventListener('click', ()=>{
-    displayTotalEmp();
-});
-
-
-
-function nbrHr(){
-
-    let cpt = 0;
-
-    emp.forEach((element)=>{
-        if(element.departement === 'HR' ){
-            cpt++;
-        }
-    });
-
-    return cpt;
-}
-
-
-function nbrLog(){
-
-    let cpt = 0;
-
-        emp.forEach((element)=>{
-        if(element.departement === 'Logistics' ){
-            cpt++;
-        }
-    });
-
-    return cpt;
-}
-
-
-function nbrPro(){
-
-    let cpt = 0;
-
-        emp.forEach((element)=>{
-        if(element.departement === 'Projects' ){
-            cpt++;
-        }
-    });
-
-    return cpt;
-}
-
-
-function nbrTec(){
-
-    let cpt = 0;
-
-        emp.forEach((element)=>{
-        if(element.departement === 'Technical' ){
-            cpt++;
-        }
-    });
-
-    return cpt;
-}
-
-
-function nbrF(){
-
-    let cpt = 0;
-
-
-   emp.forEach((element)=>{
-        if(element.departement === 'Finance' ){
-            cpt++;
-        }
-    });
-
-    return cpt;
-}
-
-
-
-function displayNbr(){
-
-    let nbrH = nbrHr();
-    let nbrP = nbrPro();
-    let nbrFi = nbrF();
-    let nbrL = nbrLog();
-    let nbrT = nbrTec();
-
-
-    document.querySelector(".nbr-hr").innerHTML = `${nbrH}`;
-    document.querySelector(".nbr-pro").innerHTML = `${nbrP}`;
-    document.querySelector(".nbr-log").innerHTML= `${nbrL}`;
-    document.querySelector(".nbr-fi").innerHTML = `${nbrFi}`;
-    document.querySelector(".nbr-tec").innerHTML = `${nbrT}`;
-}
-
-displayNbr();
