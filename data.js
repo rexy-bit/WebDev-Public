d4e11d0104eed98e39d0cb7cd1f471fd68ba64c3
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-07-18 22:26:29 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const data = [
+const defaultData = [
     {
         code:  0,
         image : 'images/iphone16.jpg',
@@ -6,6 +6,7 @@ const data = [
         note : 4.5,
         description : 'L’iPhone 16 combine performance, design et intelligence pour répondre aux besoins du quotidien. Équipé d’un écran OLED lumineux de 6,3 pouces, il offre une immersion visuelle remarquable, idéale pour le streaming, les jeux et la navigation. Sa puce A18 Bionic assure une rapidité impressionnante dans toutes les tâches, tandis que le double capteur photo 48 MP capture des images nettes et détaillées, même en basse lumière. Grâce au port USB‑C, la recharge est plus rapide et plus pratique, et l’autonomie a été optimisée pour durer toute la journée. L’iPhone 16 tourne sous iOS 18 avec des fonctions avancées d’intelligence artificielle, de sécurité renforcée et de personnalisation, le tout dans un design épuré en aluminium recyclable, disponible en plusieurs coloris modernes.',
         prix : 870,
+        stock : 3,
         categorie : "mobile",
         keyWords :  ["iphone", "apple","iPhone 16",
       "Apple iPhone 16",
@@ -25,6 +26,7 @@ const data = [
         note : 4,
         description : 'Le Dell Inspiron 15 3535 est un ordinateur portable élégant et performant conçu pour répondre aux besoins du quotidien, que ce soit pour le travail, les études ou les loisirs. Doté d’un écran 15,6 pouces Full HD (1920 x 1080), il offre une excellente qualité d’affichage pour le multimédia et la bureautique. Son puissant processeur AMD Ryzen 5 7520U, associé à la carte graphique AMD Radeon, garantit une navigation fluide, un multitâche efficace et une bonne réactivité. Il est équipé de 8 Go de RAM et d’un SSD de 512 Go, assurant un démarrage rapide et un espace de stockage confortable pour vos fichiers. Livré avec Windows 11 Home préinstallé, ce modèle intègre un clavier AZERTY français ergonomique et une finition noire sobre et professionnelle, idéale pour un usage quotidien à la maison ou au bureau.',
         prix : 1035,
+        stock : 2,
         categorie : 'pc',
         keyWords :  [  "Dell Inspiron 15",
   "Inspiron 3535",
@@ -50,30 +52,7 @@ const data = [
         note : 4.5,
         description : "Le nouveau MacBook Air 13 pouces avec puce Apple M4 redéfinit la performance et l\'efficacité dans un design ultra-fin. Spécialement conçu pour tirer parti d'Apple Intelligence, il offre une expérience fluide, intelligente et réactive au quotidien. Son superbe écran Liquid Retina de 13,6 pouces affiche des couleurs éclatantes et des détails précis, idéal pour le travail créatif, les études ou le divertissement. Grâce à ses 16 Go de mémoire unifiée et son SSD de 256 Go, ce MacBook Air garantit un multitâche sans compromis et un démarrage instantané. Il intègre également Touch ID pour une sécurité renforcée et une connexion rapide. Sa finition élégante Minuit ajoute une touche de raffinement, le tout dans un boîtier léger et silencieux, parfait pour une mobilité optimale.",
         prix : 1250,
-        categorie : 'pc',
-        keyWords :  [   "MacBook Air M4",
-  "Apple MacBook 13 pouces",
-  "ordinateur portable Apple",
-  "MacBook M4 puce",
-  "Apple Intelligence",
-  "écran Liquid Retina",
-  "MacBook Air 2025",
-  "16 Go RAM unifiée",
-  "256 Go SSD",
-  "Touch ID MacBook",
-  "Apple Minuit",
-  "Mac portable Apple",
-  "ordinateur étudiant Apple",
-  "MacBook bureautique",
-  "MacBook pour montage vidéo"]
-
-    },{
-        code:  3,
-        image : 'images/macBook1.jpg',
-        nom: 'Apple MacBook Air 13 Pouces M4',
-        note : 4.5,
-        description : "Le nouveau MacBook Air 13 pouces avec puce Apple M4 redéfinit la performance et l\'efficacité dans un design ultra-fin. Spécialement conçu pour tirer parti d'Apple Intelligence, il offre une expérience fluide, intelligente et réactive au quotidien. Son superbe écran Liquid Retina de 13,6 pouces affiche des couleurs éclatantes et des détails précis, idéal pour le travail créatif, les études ou le divertissement. Grâce à ses 16 Go de mémoire unifiée et son SSD de 256 Go, ce MacBook Air garantit un multitâche sans compromis et un démarrage instantané. Il intègre également Touch ID pour une sécurité renforcée et une connexion rapide. Sa finition élégante Minuit ajoute une touche de raffinement, le tout dans un boîtier léger et silencieux, parfait pour une mobilité optimale.",
-        prix : 1250,
+        stock : 1,
         categorie : 'pc',
         keyWords :  [   "MacBook Air M4",
   "Apple MacBook 13 pouces",
@@ -95,9 +74,10 @@ const data = [
         code:  4,
         image : 'images/appleWatch1.jpg',
         nom: 'Apple Watch Series 10 (42 mm GPS)',
-        note : 5,
+        note : 3.5,
         description : "L’Apple Watch Series 10 (42 mm GPS) allie élégance, performance et engagement environnemental. Dotée d’un boîtier en aluminium noir de jais et d’une boucle sport encre violette, elle offre un style moderne et raffiné adapté à toutes les occasions. Grâce à ses fonctionnalités avancées, cette montre connectée vous accompagne dans votre quotidien : suivi précis de l’activité physique, surveillance de la fréquence cardiaque, application ECG, et bien plus encore. Son autonomie optimisée et son intégration parfaite avec l’écosystème Apple la rendent indispensable, aussi bien pour les sportifs que pour les utilisateurs soucieux de leur bien-être. Conçue avec des matériaux neutres en carbone, elle reflète également un choix responsable en faveur de l’environnement.",
         prix : 850,
+        stock : 4,
         categorie : 'watch',
         keyWords :  ["Apple Watch Series 10",
   "montre connectée Apple",
@@ -122,6 +102,7 @@ const data = [
         note : 5,
         description : "L’Apple Watch SE (2ᵉ génération) 40 mm est une montre connectée polyvalente au design élégant, dotée d’un boîtier en aluminium argenté et d’une boucle sport nuée bleue confortable. Elle offre un suivi précis de votre activité physique, de votre sommeil, et peut détecter les chutes et accidents grâce à ses capteurs intelligents. Idéale pour un usage quotidien, elle intègre la technologie GPS, fonctionne parfaitement avec votre iPhone, et est conçue de manière responsable avec une empreinte carbone neutre. Cette montre est le choix parfait pour celles et ceux qui recherchent performance, style et bien-être au poignet.",
         prix : 990,
+        stock : 1,
         categorie : 'watch',
         keyWords :  [ "Apple Watch SE 2",
   "Apple Watch SE 2e génération",
@@ -146,6 +127,7 @@ const data = [
         note : 5,
         description : "Le Samsung Galaxy S25+ est un smartphone Android 5G haut de gamme qui allie puissance, intelligence et élégance. Doté de la technologie Galaxy AI, il optimise chaque tâche du quotidien grâce à l’intelligence artificielle embarquée. Avec ses 256 Go de stockage, il offre amplement d’espace pour vos fichiers, photos et vidéos, tandis que son design raffiné en finition gris métallisé séduit par sa modernité. Équipé d’un chargeur secteur rapide 25W inclus, il garantit une recharge efficace et rapide pour rester toujours connecté. Déverrouillé et prêt à l’emploi, le Galaxy S25+ est le compagnon idéal pour une expérience mobile fluide, intelligente et performante.",
         prix : 1022,
+        stock : 2,
         categorie : 'mobile',
         keyWords :  [  "Samsung Galaxy S25+",
   "smartphone 5G",
@@ -159,4 +141,4 @@ const data = [
   "mobile Samsung haut de gamme"]
 
     }
-]
+];
