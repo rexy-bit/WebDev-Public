6b83cff20790fa7634fa5052ded23afa750ce9b0
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-07-29 23:20:40 +0100

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import Header from './Components/Header.jsx'
-import Main from './Components/Main.jsx';
-
+import Main from './Components/Main.jsx'
 function App() {
+
   return(
-    <>
+    <main>
       <Header/>
       <Main/>
-    </>
-  );
+    </main>
+  )
 }
 
 export default App
