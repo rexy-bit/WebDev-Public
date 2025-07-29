ee6dc587faced4751f8353145180651df4c2fbaf
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-07-29 23:21:45 +0100

    Add files via upload

--- Recipe.jsx
+++ Recipe.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import ReactMarkdown from 'https://esm.sh/react-markdown@7'
-
 function Recipe(props){
 
-
     return(
-     
         
-        
-                <section className="recipe" aria-live="polite">
-                      <h2>Chef Claude recommends : </h2>
-                       <ReactMarkdown>{props.recipe}</ReactMarkdown>
-                    </section>
 
+         props.recipe &&  <div className="recipe">
+            <h2>Chef Gaston recommends :</h2>
+            <ReactMarkdown>{props.recipe}</ReactMarkdown>
+           </div>
+        
     );
 }
-
 
 export default Recipe
