ebb7d84f204948ac209ed396a8a20ee1207c31ca
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-07-29 23:18:51 +0100

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,41 +1,14 @@
-import React from 'react';
+import { useState } from 'react'
+import Header from './Components/Header.jsx'
+import Main from './Components/Main.jsx';
 
-
-function Count({number}){
-  return(
-    <h2>{number}</h2>
-  )
-}
 function App() {
-    const [count , setCount] = React.useState(0);
-
-
-    function increaseCount(){
-
-      setCount(prevCount=> prevCount + 1);
-    }
-
-    function decreaseCount(){
-      setCount(prevCount=> prevCount - 1);
-    }
-    return(
-
-      <main>
-        <div className="counter">
-          <button className="minus"
-           onClick={decreaseCount}
-          >-</button>
-          <Count
-           number={count}
-          />
-          <button className='plus'
-           onClick={increaseCount}
-          >
-            +
-          </button>
-        </div>
-      </main>
-    )
+  return(
+    <>
+      <Header/>
+      <Main/>
+    </>
+  );
 }
 
-export default App;
+export default App
