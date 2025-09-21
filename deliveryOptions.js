2ad95470943cca608eb5135985bb5815a0dd6469
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-09-21 22:58:13 +0100

    Add files via upload

---
+++ deliveryOptions.js
@@ -0,0 +1,18 @@
+export const deliveryOptions =  [
+    {
+        id : "standard",
+        name : "FREE Shipping",
+        delayDays : 5,
+        price : 0
+    },{
+        id : "express", 
+        name : "600 Dzd - Shipping",
+        delayDays : 2,
+        price : 600
+    },{
+        id : "nextDay",
+        name : "900 Dzd - Shipping",
+        delayDays : 1,
+        price : 900
+    }
+]
