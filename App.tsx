676b64bf1aceb4dea1f95dd98791bab50cb19d08
Yanis Rezgui <yanisrezgui28@gmail.com>
2026-01-20 00:53:38 +0100

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,194 +1,61 @@
-
-import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import { ItemsProvider } from './Contexts/ItemsContext'
-import { AuthProvider } from './Contexts/AuthContext'
-import UserRoute from './Layouts/UserRoute'
-import PublicLayout from './Layouts/PublicLayout'
+import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
-import Store from './Pages/Store'
-import Description from './Pages/Description'
-import Orders from './Pages/Orders'
-import Cart from './Pages/Cart'
-import AdminRoute from './Layouts/AdminRoute'
-import AdminLayout from './Layouts/AdminLayout'
-import { SearchProvider } from './Contexts/SearchContext'
-import Search from './Pages/Search'
+import Header from './Components/HomeComponents/Header'
 import Profile from './Pages/Profile'
-import SignUp from './Pages/SignUp'
-import SignIn from './Pages/SignIn'
+import { AuthProvider } from './Contexts/AuthContexts'
+import { ItemsProvider } from './Contexts/ItemsContexts'
+import Store from './Pages/Store'
 import { CartProvider } from './Contexts/CartContext'
-import { OrderProvider } from './Contexts/OrderContext'
-import Track from './Pages/Track'
-import Dashboard from './AdminPages/Dashboard'
-import AdminOrders from './AdminPages/AdminOrders'
-import Users from './AdminPages/Users'
-import AdminStore from './AdminPages/AdminStore'
-import AdminProfile from './AdminPages/AdminProfile'
-import Update from './AdminPages/Update'
-import Add from './AdminPages/Add'
-import { UsersProvider } from './Contexts/UsersContext'
-import { OrderAdminProvider } from './Contexts/OrderAdminContext'
-import GeneralFooter from './Components/HomeComponents.tsx/GeneralFooter'
+import Cart from './Pages/Cart'
 
 function App() {
+  
 
-  return (
+
+
+  return(
+    
     <AuthProvider>
-    <ItemsProvider>
-      <SearchProvider>
-         <CartProvider>
-        <OrderProvider>
-          <UsersProvider>
-            <OrderAdminProvider>
+      <ItemsProvider>
+        <CartProvider>
+     <Routes>
+      
+
           
-      <Routes>
+        <Route path='/' element={
+          <>
+          <Header/>
+          <Home/>
+          </>
+        }/>
 
-         <Route element={
-          <UserRoute>
-            <PublicLayout/>
-          </UserRoute>
-         }>
+        <Route path='/profile' element={
+          <>
+            <Header/>
+            <Profile/>
+          </>
+        }/>
 
-         
-
-           <Route path='/' element={
-            <>
-               
-               <Home/>
-            </>
-           }/>
-
-           <Route path='/store' element={
-            <>
-             
-            <Store/>
-            <GeneralFooter/>
-            </>
-           }/>
-
-           <Route path='/description' element={
-            <>
-            <Description/>
-            <GeneralFooter/>
-            
-                </>
-           }/>
-
-           <Route path='/orders' element={
-            <>
-            
-            <Orders/>
-            <GeneralFooter/>
-            </>
-
-           }/>
-
-            <Route path='/track/:id' element={
-              <>
-                
-                <Track/>
-                <GeneralFooter/>
-              </>
-            }/>
-           <Route path='/cart' element={
-            <>
-            
-            <Cart/>
-            <GeneralFooter/>
-            </>
-           }/>
-     
-           <Route path="/search" element={
-            <>
-              
-              <Search/>
-              <GeneralFooter/>
-            </>
-           }/>
-
-             <Route path='/description/:id' element={
-              <>
-                
-                <Description/>
-                <GeneralFooter/>
-              </>
-            }/>
-            
-            <Route path='/profile' element={
-              <>
-                
-                <Profile/>
-                <GeneralFooter/>
-              </>
-            }/>
-
-            <Route path='/signUp' element={
-              <>
-                
-                <SignUp/>
-                <GeneralFooter/>
-              </>
-            }/>
-
-            <Route path='/signIn' element={
-              <>
-                
-                <SignIn/>
-                <GeneralFooter/>
-              </>
-            }/>
-
-            </Route>
-
-
-            <Route path='/admin/*' element={
-              <AdminRoute>
-                <AdminLayout/>
-              </AdminRoute>
-            }>
-              
-              <Route path='dashboard' element={
-                <Dashboard/>
-              }/>
-
-               <Route path='orders' element={
-                 <AdminOrders/>
-              }/>
- 
-                 <Route path='users' element={
-                <Users/>
-              }/>
-
-              <Route path='store' element={
-                <AdminStore/>
-              }/>
-              
-               <Route path='profile' element={
-                <AdminProfile/>
-              }/>
-
-              <Route path='update/:id' element={
-                <Update/>
-              }/>
-
-              <Route path='add' element={
-                <Add/>
-              }
-              />
-            </Route>
-
-      </Routes>
-
-
-                </OrderAdminProvider>
-             </UsersProvider>
-          </OrderProvider>
-        </CartProvider>
+        <Route path='/store' element={
+          <>
+          <Header/>
+          <Store/>
+          </>
+        }/>
         
-       </SearchProvider>
-    </ItemsProvider>
-    </AuthProvider>
+        <Route path='/cart' element={
+
+          <>
+            <Header/>
+            <Cart/>
+          </>
+        }/>
+     </Routes>
+
+           </CartProvider>
+        </ItemsProvider>
+     </AuthProvider>
   )
 }
 
