1c76b1e0a8d472be82563e905e0f456314bed5f3
Yanis Rezgui <yanisrezgui28@gmail.com>
2026-01-27 00:45:28 +0100

    Add files via upload

---
+++ AdminOrders.tsx
@@ -0,0 +1,13 @@
+import  { memo } from "react"
+
+
+const AdminOrders = () => {
+
+    return(
+        <section>
+            Orders
+        </section>
+    )
+}
+
+export default memo(AdminOrders);
