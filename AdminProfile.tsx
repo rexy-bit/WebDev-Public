ef6aa6854eb5a994ebc926f86cac5e040d2c9d84
Yanis Rezgui <yanisrezgui28@gmail.com>
2026-01-27 00:45:35 +0100

    Add files via upload

---
+++ AdminProfile.tsx
@@ -0,0 +1,14 @@
+import  { memo } from "react"
+
+
+
+const AdminProfile = () => {
+
+    return(
+        <section>
+            Admin Profile
+        </section>
+    )
+}
+
+export default memo(AdminProfile);
