33d32dfaca6283ec141d3b06d384779fd605b685
Yanis Rezgui <yanisrezgui28@gmail.com>
2026-02-04 23:42:31 +0100

    Add files via upload

--- Home.tsx
+++ Home.tsx
@@ -1,68 +1,28 @@
 import  { memo } from "react"
-import Hero from "../Components/HomeComponents/Hero";
-import About from "../Components/HomeComponents/About";
-import Why from "../Components/HomeComponents/Why";
-import {motion} from "framer-motion"
-import Best from "../Components/HomeComponents/Best";
-import Categories from "../Components/HomeComponents/Categories";
-import Testimonials from "../Components/HomeComponents/Testimonials";
-import Faq from "../Components/HomeComponents/Faq";
-import Contact from "../Components/HomeComponents/Contact";
-import Footer from "../Components/HomeComponents/Footer";
+import Header from "../Components/Header";
+
+import Hero from "../Components/Hero";
+import About from "../Components/About";
+import Programs from "../Components/Programs";
+import WhyChoose from "../Components/WhyChoose";
+import MainTestimonials from "../Components/MainTestimonials";
+import MainFaq from "../Components/MainFaq";
+import Contact from "../Components/Contact";
 
 
 const Home = () => {
 
-    const sectionVariants = {
-  hidden: { opacity: 0, y: 50 },  // invisible & déplacé vers le bas
-  visible: { 
-    opacity: 1, 
-    y: 0,
-    transition: { duration: 0.6, ease: "easeOut" } 
-  },
-};
-
-
     return(
-         <section className="flex flex-col w-full items-center">
-            <motion.div
-        variants={sectionVariants}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.3 }}
-        className="w-full"
-      >
-             <Hero/>
-             </motion.div>
-
-             <motion.div
-        variants={sectionVariants}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.3 }}
-        className="w-full"
-      >
-             <About/>
-             </motion.div>
-
-             
-              <motion.div
-        variants={sectionVariants}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.3 }}
-        className="w-full"
-      >
-             <Why/>
-             </motion.div>
-
-             <Best/>
-             <Categories/>
-             <Testimonials/>
-             <Faq/>
-             <Contact/>
-             <Footer/>
-         </section>
+        <>
+          <Header/>
+          <Hero/>
+          <About/>
+          <Programs/>
+          <WhyChoose/>
+          <MainTestimonials/>
+          <MainFaq/>
+          <Contact/>
+        </>
     )
 }
 
